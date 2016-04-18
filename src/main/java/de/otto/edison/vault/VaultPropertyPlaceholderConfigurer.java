945c9df0192c10bd5d659c9132a5a8eb85ff94a5
Matthias Bargmann <matthias.bargmann@ottogroup.com>
2016-04-18 09:40:54 +0200

    do not create vaultclient if disabled via property

--- src/main/java/de/otto/edison/vault/VaultPropertyPlaceholderConfigurer.java
+++ src/main/java/de/otto/edison/vault/VaultPropertyPlaceholderConfigurer.java
@@ -2,12 +2,13 @@ package de.otto.edison.vault;
 
 import com.ning.http.client.AsyncHttpClient;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import static de.otto.edison.vault.VaultClient.vaultClient;
 
 @Component
 @ConditionalOnProperty(prefix = "edison.vault", name = "enableconfigurer", matchIfMissing = true)
@@ -18,8 +19,8 @@ public class VaultPropertyPlaceholderConfigurer extends PropertySourcesPlacehold
     @Override
     public void postProcessBeanFactory(final ConfigurableListableBeanFactory beanFactory) throws BeansException {
         ConfigProperties configProperties = new ConfigProperties(environment);
-        VaultReader vaultReader = new VaultReader(configProperties, VaultClient.vaultClient(configProperties, new VaultToken(configProperties, new AsyncHttpClient())));
-        if (vaultReader.vaultEnabled()) {
+        if (configProperties.isEnabled()) {
+            VaultReader vaultReader = new VaultReader(configProperties, vaultClient(configProperties, new VaultToken(configProperties, new AsyncHttpClient())));
             setProperties(vaultReader.fetchPropertiesFromVault());
         }
         super.postProcessBeanFactory(beanFactory);
